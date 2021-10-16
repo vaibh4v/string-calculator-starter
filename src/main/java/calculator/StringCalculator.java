c54f628126dfbbe44132a88bd0db2cc1865fa2ee
vaibh4v <vaibhavdukare911@gmail.com>
2021-10-16 21:50:53 +0530

    tweaked a method to handle string having multiple numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,18 +2,29 @@ package calculator;
 
 class StringCalculator
 {
-
     public int add(String input)
     {
-    	// returning 0 if string is empty
     	if(input.isEmpty())
     	{
+    		// returning 0 if string is empty
     		return 0;
     	}
-    	
-    	
-    	
-        return 0;
+    	else if(input.contains(","))
+    	{
+    		String[] tokens = input.split(",");
+    		
+    		int sum = 0;
+    		
+    		for(String num : tokens)
+    		{
+    			sum += Integer.parseInt(num);
+    		}
+    		
+    		return sum;
+    	}
+    	else
+    	{
+    		return Integer.parseInt(input);
+    	}
     }
-
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -7,14 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorShould {
 
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0()
+    {
         StringCalculator stringCalculator = new StringCalculator();
+        
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int()
+    {
         StringCalculator stringCalculator = new StringCalculator();
+        
         assertEquals(1, stringCalculator.add("1"));
+    }
+    
+    @Test
+    void string_with_two_numbers_separated_with_commas_should_return_their_sum()
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	
+    	assertEquals(5, stringCalculator.add("2,3"));
+    }
+    
+    @Test
+    void string_with_multiple_numbers_separated_with_commas_should_return_their_sum()
+    {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	
+    	assertEquals(25, stringCalculator.add("2,3,4,6,10"));
     }
 }
