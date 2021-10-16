e6f23d5cb3935fbe558399475a61ba43fd0a01fa
vaibh4v <vaibhavdukare911@gmail.com>
2021-10-16 19:37:47 +0530

    added method to handle empty string

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,18 @@
 package calculator;
 
-class StringCalculator {
+class StringCalculator
+{
 
-    public int add(String input) {
+    public int add(String input)
+    {
+    	// returning 0 if string is empty
+    	if(input.isEmpty())
+    	{
+    		return 0;
+    	}
+    	
+    	
+    	
         return 0;
     }
 
